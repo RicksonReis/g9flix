497da38abbaf3b82a79221e5be6e8475c4308b0b
Rickson <cyberickson@gmail.com>
2020-08-04 07:35:50 -0300

    Aula 04 finalizada

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -27,8 +27,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    console.log('alo alo');
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias'
+      : 'https://g9flix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
