d880f7dfddff933412bb58511f542d44bf122f8b
Rickson <cyberickson@gmail.com>
2020-08-04 20:51:56 -0300

    Update pessoal

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import IconeFooter from '../../assets/img/Logo.png';
+import './styles.css';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://github.com/emersondevelops">
+        <img className="iconeFooter" src={IconeFooter} alt="Github do desenvovedor" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Desenvolvido por
+        {' '}
+        <a href="https://www.linkedin.com/in/rickson-reis-g9/">Rickson Reis</a>
+        {' '}
+        durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
